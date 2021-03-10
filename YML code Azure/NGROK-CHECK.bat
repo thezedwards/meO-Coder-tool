618b69947ed9e63cf61d0d2ead4ad42d179ff6ec
dangvinh <dangvinhprovn@gmail.com>
2021-03-10 17:11:45 +0700

    Update NGROK-CHECK.bat

--- YML code Azure/NGROK-CHECK.bat
+++ YML code Azure/NGROK-CHECK.bat
@@ -10,7 +10,7 @@ echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Reru
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, please paste new NGROK TOKEN in YML. Check Tunnel here: https://dashboard.ngrok.com/status/tunnels " && exit 
 echo User: Administrator
-echo Pass: Thuonghai001
+echo Pass: Dvinh99bn
 echo Subscribe: https://www.youtube.com/channel/UCi80Ipndm3QovIn5XisUz3g
 curl -O https://gitlab.com/haithuongpro2711/haivps/-/raw/master/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://gitlab.com/haithuongpro2711/haivps/-/raw/master/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
